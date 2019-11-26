95d5822868e03a767608a88a50596ae1d0d893b2
Elvijs <elvijs.vilkels@live.com>
2019-11-26 19:41:41 +0100

    Form Stepper

---
+++ src/components/pages/Form.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FormExample = () => {
+  return (
+    <div>
+      <h2>Form example {this.props.id}</h2>
+    </div>
+  );
+};
+
+export default FormExample;
--- src/components/pages/VisaPage/VisaPage.jsx
+++ src/components/pages/VisaPage/VisaPage.jsx
@@ -1,10 +1,26 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {HorizontalStepper} from "./Stepper";
+import FormExample from "../Form";
+
+const stepForms = (stepIndex) => {
+  return (<FormExample id={stepIndex}/>);
+};
+
+const getSteps = () => {
+  return ['Basic info', 'Additional info', 'Verification', "Submission"];
+};
 
 class VisaPage extends Component {
-    state = {  }
-    render() { 
-        return <div> I am visa </div>;
-    }
+  state = {activeForm: 0};
+
+  render() {
+    const {activeForm} = this.state;
+    return (
+      <HorizontalStepper stepContent={stepForms} stepInfo={getSteps}/>
+    );
+
+
+  }
 }
- 
+
 export default VisaPage;
